10fcfaf214d9ddecaf194a694c1dc06e54fb44f5
kkkirill <kkg.410@mail.ru>
2019-05-24 17:56:56 +0300

    bugfix

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ class CalculatorConfig:
     def __init__(self):
         self.amount_of_blocks = 6
         self.spin_box_defaults = (0.6, 0.18, 0.19, 0.2, 0.7, 0.12)
-        self.spin_box_qualification_defaults = (6, 9)
+        self.spin_box_qualification_defaults = [6, 9]
         self.edit_line_default = ''
 
 
@@ -23,14 +23,16 @@ class Calculator(QDialog):
         self.setFixedSize(783, 574)
         self.spin_values = [value for value in self.config.spin_box_defaults]
         self.spin_values_qualification = self.config.spin_box_qualification_defaults
+        self.__reset(False)
         self.__bind()
 
-    def __reset(self):
-        for i, spin_box_value in enumerate(self.config.spin_box_defaults, start=1):
-            getattr(self, f'spinBox{i}').setValue(spin_box_value)
+    def __reset(self, flag: bool = True):
+        for i, spin_box_value in enumerate(self.config.spin_box_defaults if flag else self.spin_values, start=1):
+            getattr(self, f'doubleSpinBox{i}').setValue(spin_box_value)
         for i in range(int(self.config.amount_of_blocks / 2)):
-            getattr(self, f'lineEdit{i}').setText(self.config.edit_line_default)
-        for i, value in enumerate(self.config.spin_box_qualification_defaults, start=1):
+            getattr(self, f'lineEdit{i+1}').setText(self.config.edit_line_default)
+        for i, value in enumerate(
+                self.config.spin_box_qualification_defaults if flag else self.spin_values_qualification, start=1):
             getattr(self, f'spinBox{i+6}').setValue(value)
         self.__update()
 
@@ -43,25 +45,25 @@ class Calculator(QDialog):
     def __update(self):
         for i in range(self.config.amount_of_blocks):
             self.spin_values[i] = getattr(self, f'doubleSpinBox{i+1}').value()
-        for i in range(len(self.spin_values_qualification)):
+        for i, _ in enumerate(self.spin_values_qualification):
             self.spin_values_qualification[i] = getattr(self, f'spinBox{i+7}').value()
 
     def __calculate(self):
         self.__update()
         self.weights = [value / sum(self.spin_values_qualification) for value in self.spin_values_qualification]
         for i in range(1, 3):
-            getattr(self, f'lineEdit{i}').setText(round(self.weights[i], 2))
-        self.output()
-        self.massageBox.show()
+            getattr(self, f'lineEdit{i}').setText(str(round(self.weights[i - 1], 2)))
+        result = [round(self.weights[0] * self.spin_values[i] + self.weights[1] * self.spin_values[i + 3], 3) for i in
+                  range(int(len(self.spin_values) / 2))]
+        self.output(result)
 
-    def output(self):
-        pass
+    def output(self, result: list):
+        getattr(self, f'lineEdit3').setText(str(result))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Calculator()
     window.show()
-    
 
     sys.exit(app.exec_())
